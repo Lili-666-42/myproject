8a294bdcd759e6f21ca14b7b7bdc20c221c68741
Lili-666-42 <ssyp06510@gmail.com>
2024-02-25 15:52:12 +0300

    sec

---
+++ myproject/myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+
+def index(request):
+    items = Item.objects.all()
+    return render(request, 'myapp/index.html', {'items': items})
